ce39cacd5fbfe303bcba10fef2f0c25116affd67
Ilya Volkov <volkovdevelop@vk.com>
2024-11-14 11:28:26 +0300

    deletes deploy.sh

--- ivanor-test-task/vite.config.js
+++ ivanor-test-task/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: '/ivanor-test-task/',
+  publicPath: '/',
   plugins: [
     vue(),
   ],
