4936f87f82ac81f08c4996e993158512d7a6519b
Ilya Volkov <volkovdevelop@vk.com>
2024-11-14 11:41:44 +0300

    changes vite config

--- ivanor-test-task/vite.config.js
+++ ivanor-test-task/vite.config.js
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-  ? '/ivanor-test-task/'
-  : '/',
+  base: '/ivanor-test-task/',
   plugins: [
     vue(),
   ],
